20d68f399be81d196edd4f6e4034db0475b22077
Camara Mohamed Salim <m.salimcamara@gmail.com>
2022-03-12 18:55:13 +0100

    Commit d'initialisation, ajout de la classe de récupération des datas

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,45 +1,58 @@
-    async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+// fonction de récupération de la data
+let dataPhotographers = null;
+let dataMedia = null;
+const url = '../../data/photographers.json';
+
+class Fetchdata {
+    constructor(domain) {
+        this.domain = domain;
     }
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+    // récupération des données photographes
+    async photographers() {
+        await fetch(this.domain)
+            .then((res) => {
+                return res.json()
+            })
+            .then((data) => {
+                dataPhotographers = data.photographers
+            })
+            .catch((err) => console.log('erreur récupération data ' + err))
+    }
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
+    // récupération des images etc.
+    async media() {
+        await fetch(this.domain)
+            .then((res) => {
+                return res.json()
+            })
+            .then((data) => {
+                dataMedia = data.media
+            })
+            .catch((err) => console.log('erreur récupération data ' + err))
+    }
+}
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
-    
-    init();
+const photographers = new Fetchdata(url).photographers();
+const media = new Fetchdata(url).media();
+
+
+async function displayData(dataPhotographers) {
+    const photographersSection = document.querySelector(".photographer_section");
+
+    dataPhotographers.forEach((photographer) => {
+        const photographerModel = photographerFactory(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+};
+
+async function init() {
+    displayData(dataPhotographers);
+};
+
+photographers.then(() => init());
+
+
+
     
