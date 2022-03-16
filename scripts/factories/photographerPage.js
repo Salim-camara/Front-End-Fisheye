88a878c1f9edcf346c4c0d1f7a06bf78d9b51491
Camara Mohamed Salim <m.salimcamara@gmail.com>
2022-03-16 16:37:50 +0100

    Fin de la partie headerPhotograph

--- scripts/factories/photographerPage.js
+++ scripts/factories/photographerPage.js
@@ -1,9 +1,19 @@
 const photographInfo = (data) => {
-    const { title, portrait, city, country, tagline, price, name } = data
+    const { portrait, city, country, tagline, name } = data;
+    const picture = `assets/photographers/${portrait}`;
 
     const titlePhotograph = document.querySelector('.photograph-info--title');
     titlePhotograph.innerHTML = `${name}`;
-    titlePhotograph.style.color = 'green';
+
+    const locationPhotograph = document.querySelector('.photograph-info--location');
+    locationPhotograph.innerHTML = `${city}, ${country}`;
+
+    const taglinePhotograph = document.querySelector('.photograph-info--desc');
+    taglinePhotograph.innerHTML = `${tagline}`;
+
+    const imgPhotograph = document.querySelector('.photograph-info--img');
+    imgPhotograph.setAttribute("src", picture);
+    imgPhotograph.style.borderRadius = '50%';
 }
 
 
